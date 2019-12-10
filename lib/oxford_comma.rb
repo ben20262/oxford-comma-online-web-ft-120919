46ac00a0b291f0c24c858b52d0d86ad3f214720d
Benjamin Small <ben20262@aim.com>
2019-12-10 22:09:19 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-  array.join(", ")
+  words = ""
+  if array.size > 2
+    array.each do |word|
+      if word == array[-1]
+        words = words + ", and" + word
+      else
+        words = words + "," + word
+      end
+    end
+  elsif array.size == 2 
+    array.join(" and ")
+  else
+    array.flatten
+  end
 end
